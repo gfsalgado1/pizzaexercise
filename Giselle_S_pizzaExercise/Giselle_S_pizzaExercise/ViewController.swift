4823631a8f713c5f82c0047da897d747d4f8a463
gfsalgado1 <52936092+gfsalgado1@users.noreply.github.com>
2020-08-03 14:52:28 -0500

    first commit
    just the lay out design

---
+++ Giselle_S_pizzaExercise/Giselle_S_pizzaExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Giselle_S_pizzaExercise
+//
+//  Created by Giselle Salgado on 8/3/20.
+//  Copyright © 2020 Giselle Salgado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
